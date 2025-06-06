87d78650aa6e9ac27d89c6f23d56bee99638b888
mariabalbis <162700638+mariabalbis@users.noreply.github.com>
2025-06-06 11:24:24 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BanhoTosa from "./pages/BanhoTosa";
+import HotelResort from "./pages/HotelResort";
+import Adestramento from "./pages/Adestramento";
+import NavbarMenu from "./pages/NavbarMenu";
+import Footer from "./components/Footer";
+
+const App = () => {
+	return (
+		<Router>
+			<header>
+				<NavbarMenu />
+			</header>
+
+			<main>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/banho-e-tosa" element={<BanhoTosa />} />
+				<Route path="/hotel-resort" element={<HotelResort />} />
+				<Route path="/adestramento" element={<Adestramento />} />
+			</Routes>
+			</main>
+
+			<footer>
+				<Footer />
+			</footer>
+		</Router>
+	);
+};
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import Contato from "./Contato";
+
+function Footer() {
+  return (
+    <div>
+      <Contato />
+
+      <Mapa />
+    </div>
+  );
+}
+
+export default Footer;
